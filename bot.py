2ce62d41a23bddeb3367c89eb7528027dd6093d2
lawvs <lawvs@users.noreply.github.com>
2019-01-30 05:26:26 +0800

    feat: custom message template

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import telegram
-from textwrap import dedent
 import logging
 
 from config import config
@@ -8,33 +7,30 @@ logger = logging.getLogger(__name__)
 
 
 def formatRss(subInfo, item) -> str:
-    return dedent('''
-    {name}
-    {title}
-    Updated: {updated}
-    Author: {author}
-    [Read More]({link})
-    '''.format(
+    template = subInfo.get('template') or config['template']
+    return template.format(
         name=subInfo['name'],
         title=item.get('title'),
         updated=item.get('updated'),
         author=item.get('author'),
         link=item.get('link')
-    ))
+    )
 
 
 def sendRssUpdateMessage(self, configInfo, rss, **kw):
     text = formatRss(configInfo, rss)
     logger.info(text)
     chatId = configInfo.get('chat_id') or config['telegram']['default_chat_id']
+    parseMode = configInfo.get(
+        'parse_mode') or config['telegram'].get('parse_mode')
 
     if isinstance(chatId, str):
-        self.send_message(chatId, text, parse_mode='Markdown', **kw)
+        self.send_message(chatId, text, parse_mode=parseMode, **kw)
         return
 
     if isinstance(chatId, list):
         for i in chatId:
-            self.send_message(i, text, parse_mode='Markdown', **kw)
+            self.send_message(i, text, parse_mode=parseMode, **kw)
         return
     raise Exception('unexpected type of chat_id in {}, expected \'str|list|None\' but {}'.format(
         configInfo, type(chatId)))
